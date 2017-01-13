490fcbdeb2b961947260763c3e92432a9c184daf
Andre <Andre@DESKTOP-EVCUACT>
2017-01-13 19:49:44 -0200

    Compatibility changes

--- wi.core.treeview.module.js
+++ wi.core.treeview.module.js
@@ -1,10 +1,10 @@
 /**
- *  __          __  _    _____ _____  ______ 
- *  \ \        / / | |  |_   _|  __ \|  ____|
- *   \ \  /\  / /__| |__  | | | |  | | |__   
- *    \ \/  \/ / _ \ '_ \ | | | |  | |  __|  
- *     \  /\  /  __/ |_) || |_| |__| | |____ 
- *      \/  \/ \___|_.__/_____|_____/|______|
+ *  __          __  _    _____ _____  ______ _______                  _               
+ *  \ \        / / | |  |_   _|  __ \|  ____|__   __|                (_)              
+ *   \ \  /\  / /__| |__  | | | |  | | |__     | |_ __ ___  _____   ___  _____      __
+ *    \ \/  \/ / _ \ '_ \ | | | |  | |  __|    | | '__/ _ \/ _ \ \ / / |/ _ \ \ /\ / /
+ *     \  /\  /  __/ |_) || |_| |__| | |____ _ | | | |  __/  __/\ V /| |  __/\ V  V / 
+ *      \/  \/ \___|_.__/_____|_____/|______(_)|_|_|  \___|\___| \_/ |_|\___| \_/\_/                                                                                
  *                                                                            
  *  @author André Ferreira <andrehrf@gmail.com>
  *  @license MIT
@@ -20,7 +20,7 @@ module.exports = {
      * @type object
      */
     assets: {
-        js: [__dirname + "/mar10/fancytree/dist/jquery.fancytree-all.min.js", __dirname + "/wi.core.treeview.event.js"],
-        css: [__dirname + "/mar10/fancytree/dist/skin-awesome/ui.fancytree.min.css"]
+        js: [__dirname + "/fancytree/dist/jquery.fancytree-all.min.js", __dirname + "/wi.core.treeview.event.js"],
+        css: [__dirname + "/fancytree/dist/skin-awesome/ui.fancytree.min.css"]
     }
 };
